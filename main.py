64693a94e02b2e87a07f785c2be07ea67ae3f3d4
guo <aiguofengmail@163.com>
2025-03-07 21:08:44 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from gui.ui import PlayerUI
-from PySide6.QtWidgets import QApplication
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    player_ui = PlayerUI()
+    app, player_ui = PlayerUI.create_app()
+
+    # 如果传入文件路径,直接加载播放
+    if len(sys.argv) > 1:
+        player_ui.play_file(sys.argv[1])
+
     player_ui.show()
     sys.exit(app.exec())
